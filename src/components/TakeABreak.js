e316cafcfe32c14c05fffaa6b51c7c0d650c4bf3
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 11:48:15 +0100

    Setting up TakeABreak component

---
+++ src/components/TakeABreak.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import keepActive from '../images/keep-active.jpg'
+
+class TakeABreak extends Component {
+    render() {
+        return (
+            <div className="take-a-break-container">
+
+            </div>
+        )
+    }
+}
+
+export default TakeABreak;
