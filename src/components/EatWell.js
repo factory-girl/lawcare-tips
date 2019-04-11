1a5f6fb20c2eac9eda42a811380f90cf66db3dbb
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 14:51:08 +0100

    Adding EatWell content

--- src/components/EatWell.js
+++ src/components/EatWell.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import shareHowYouFeel from "../images/share-how-you-feel.jpg";
+import eatWell from "../images/eat-well.jpg";
 
 class EatWell extends Component {
     render() {
         return (
             <div className="eat-well-container">
-
+                <img src={eatWell}
+                     className="main-image"/>
+                <p className="bold-text">Eat Well</p>
+                <p className="content-text">A balanced diet rich in nutrients is good for both your mental and
+                    physical health</p>
             </div>
         )
     }
