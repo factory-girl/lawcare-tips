e94f538530891341988a4ebe03651e8ebdb82718
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 15:59:59 +0100

    Adding AskForHelp content

--- src/components/AskForHelp.js
+++ src/components/AskForHelp.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import beMindful from "../images/be-mindful.jpg";
+import askForHelp from "../images/ask-for-help.jpg";
 
 class AskForHelp extends Component {
     render() {
         return (
             <div className="ask-for-help-container">
-
+                <img src={askForHelp}
+                     className="main-image"/>
+                <p className="bold-text">Ask for Help</p>
+                <p className="content-text">Asking for help is a sign of strength, and can lead to the right support
+                    for you, whatever you're going through</p>
             </div>
         )
     }
