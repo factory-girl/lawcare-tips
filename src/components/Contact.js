dd388740948ef555230575efa263d724377fbacd
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 20:29:55 +0100

    Adding buttons, more contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,8 @@ class Contact extends Component {
                      className="main-image"/>
                 <p className="content-text">Call our free, independent, confidential Helpline on
                     <span className="red-text"> 0800 279 6888</span></p>
+                <p className="content-text">365 days a year </p>
+                <p className="content-text">9am – 7.30pm weekdays, 10am – 4pm weekends and Bank Holidays</p>
                 <button className="previous-button">&#8249;</button>
                 <button onClick={this.props.onContactNext}
                         className="next-button">&#8250;</button>
