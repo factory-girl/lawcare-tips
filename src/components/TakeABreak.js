a0262bffc69274231c23fb130e92a635bf926b33
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 13:29:47 +0100

    Adding TakeABreak content

--- src/components/TakeABreak.js
+++ src/components/TakeABreak.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import keepActive from '../images/keep-active.jpg'
+import takeABreak from '../images/take-a-break.jpg'
 
 class TakeABreak extends Component {
     render() {
         return (
             <div className="take-a-break-container">
-
+                <img src={takeABreak}
+                     className="main-image"/>
+                <p className="bold-text">Take a Break</p>
+                <p className="content-text">Use your lunchbreak to get away from your desk. Step outside for just a few
+                    minutes, it can re-energise you</p>
             </div>
         )
     }
