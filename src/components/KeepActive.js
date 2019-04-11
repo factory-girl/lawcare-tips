104dfc6290b52405a3a8d7ab3ae3d693465e5bf4
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 11:41:53 +0100

    Adding initial intro, template for KeepActive slide

--- src/components/KeepActive.js
+++ src/components/KeepActive.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import keepActive from '../images/keep-active.jpg'
 
 class KeepActive extends Component {
     render() {
         return (
             <div className="keepactive-container">
+                <img src={keepActive}
+                     className="main-image"/>
+                <p className="bold-text">Look After Yourself</p>
+                <p className="content-text">Find a physical activity you enjoy and make it part of your life, you
+                    will feel better and boost your self-esteem</p>
 
             </div>
         )
