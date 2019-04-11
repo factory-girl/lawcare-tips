67f0b16c0716297dc9c5a044f041481180113a9e
mckaylareilly <mckayla.reilly@clio.com>
2019-04-11 14:31:28 +0100

    Adding SleepIsImportant template

--- src/components/ShareHowYouFeel.js
+++ src/components/ShareHowYouFeel.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import sleepIsImportant from "../images/sleep-is-important.jpg";
+import shareHowYouFeel from "../images/share-how-you-feel.jpg";
 
 class ShareHowYouFeel extends Component {
     render() {
         return (
             <div className="sleep-is-important-container">
-
+                <img src={shareHowYouFeel}
+                     className="main-image"/>
+                <p className="bold-text">Share How You Feel</p>
+                <p className="content-text">Talk about your feelings, it can help you cope with problems and feel
+                    listened to</p>
             </div>
         )
     }
